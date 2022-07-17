1bd459cea6945a012a7c8e3d96332740cf30ce67
Dominux <kir.gimranov@gmail.com>
2022-07-17 13:53:52 +0300

    #11 Created strips on Y axis

--- frontend/src/goban/sphere.ts
+++ frontend/src/goban/sphere.ts
@@ -11,12 +11,9 @@ export default class Playground {
 		// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
 		const light = new BABYLON.HemisphericLight(
 			'light1',
-			new BABYLON.Vector3(0, 1, 0),
+			new BABYLON.Vector3(1, 1, 0),
 			scene
 		)
-
-		// Default intensity is 1. Let's dim the light a small amount
-		light.intensity = 0.7
 
 		// Our built-in 'sphere' shape. Params: name, options, scene
 		const sphere = BABYLON.MeshBuilder.CreateSphere(
@@ -25,20 +22,67 @@ export default class Playground {
 			scene
 		)
 
-		const cube = BABYLON.MeshBuilder.CreateBox('ur mom', {}, scene)
-		console.log(cube)
+		// Creating material
+		// Our built-in 'sphere' shape. Params: name, options, scene
+		const gridSize = 7
+		const height = 1000
+		const gridInc = height / gridSize
+		const textureGrid = new BABYLON.DynamicTexture(
+			'grid texture',
+			height,
+			scene,
+			false
+		)
 
-		// Move the sphere upward 1/2 its height
-		sphere.position.y = 1
+		const textureContext = textureGrid.getContext()
+		textureContext.fillStyle = 'white'
+		textureContext.fillRect(0, 0, height, height)
+		textureGrid.update()
 
-		console.log(sphere)
+		textureContext.lineWidth = 10 / gridSize
+		textureContext.strokeStyle = 'black'
+		textureContext.fillStyle = 'black'
+
+		const k = gridSize * 2
+		const lines = [...Array(k + 1).keys()]
+			.filter((n) => n % 2 == 1)
+			.map((n) => n / (k * 2) + 0.25)
+		console.log(lines)
+
+		for (const line of lines) {
+			const scaledLine = line * height
+
+			textureContext.moveTo(0, scaledLine)
+			textureContext.lineTo(height, scaledLine)
+			textureContext.stroke()
+		}
+
+		// for (let x = 0; x < 1; x += gridInc) {
+		// 	// Normalization
+		// 	const y = x / 2 + normalization_k
+
+		// 	console.log(y)
+
+		// 	// textureContext.beginPath()
+		// 	// textureContext.moveTo(y, 0)
+		// 	// textureContext.lineTo(y, height)
+		// 	textureContext.stroke()
+		// 	textureContext.moveTo(0, y)
+		// 	textureContext.lineTo(height, y)
+		// 	textureContext.stroke()
+		// }
+		textureGrid.update()
+
+		const mat = new BABYLON.StandardMaterial('Mat', scene)
+		mat.diffuseTexture = textureGrid
+		sphere.material = mat
 
 		const camera = new BABYLON.ArcRotateCamera(
 			'camera',
 			-Math.PI / 2,
 			Math.PI / 2,
-			5,
-			new BABYLON.Vector3(0, 1, 0),
+			3,
+			new BABYLON.Vector3(0, 0, 0),
 			scene
 		)
 
