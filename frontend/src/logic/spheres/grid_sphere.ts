1845ab8b6e3552c0c773656c6774d33b80011d42
Dominux <kir.gimranov@gmail.com>
2022-07-25 22:20:41 +0300

    #11 used snippet

--- frontend/src/logic/spheres/grid_sphere.ts
+++ frontend/src/logic/spheres/grid_sphere.ts
@@ -35,76 +35,109 @@ export default class GridSpherePlayground {
 			scene
 		)
 
-		const gridMaterial = new GridMaterial('')
+		BABYLON.NodeMaterial.ParseFromSnippetAsync(
+			// 'gridMaterial',
+			'YCMALW#7',
+			scene
+		).then((gridMaterial) => {
+			const gridRatio = gridMaterial.getInputBlockByPredicate(
+				(b) => b.name === 'gridRatio'
+			)
+			const majorUnitFrequency = gridMaterial.getInputBlockByPredicate(
+				(b) => b.name === 'majorUnitFrequency'
+			)
+			const minorUnitVisibility = gridMaterial.getInputBlockByPredicate(
+				(b) => b.name === 'minorUnitVisibility'
+			)
+			const circleRadius = gridMaterial.getInputBlockByPredicate(
+				(b) => b.name === 'circleRadius'
+			)
+			const circleAmount = gridMaterial.getInputBlockByPredicate(
+				(b) => b.name === 'circleAmount'
+			)
+			const circlePosition = gridMaterial.getInputBlockByPredicate(
+				(b) => b.name === 'circlePosition'
+			)
+			const circleSmoothness = gridMaterial.getInputBlockByPredicate(
+				(b) => b.name === 'circleSmoothness'
+			)
+			const bgColor = gridMaterial.getInputBlockByPredicate(
+				(b) => b.name === 'bgDiffuseColor'
+			)
+			const lineColor = gridMaterial.getInputBlockByPredicate(
+				(b) => b.name === 'lineColor'
+			)
 
-		gridMaterial.mainColor = BABYLON.Color3.White()
-		gridMaterial.lineColor = BABYLON.Color3.Black()
-		gridMaterial.gridRatio = 0.1
-		gridMaterial.majorUnitFrequency = 1
-		gridMaterial.minorUnitVisibility = 0
+			bgColor.value = BABYLON.Color3.White()
+			lineColor.value = BABYLON.Color3.Black()
+			gridRatio.value = 0.1
+			majorUnitFrequency.value = 1
+			minorUnitVisibility.value = 0
+			circleRadius.value = 0.03
+			circleSmoothness.value = 0.005
 
-		sphere.material = gridMaterial
-		sphere.enablePointerMoveEvents = true
+			sphere.material = gridMaterial
+			sphere.enablePointerMoveEvents = true
 
-		scene.onPointerMove = (
-			event: BABYLON.IPointerEvent,
-			pickInfo: BABYLON.PickingInfo
-		) => {
-			// if (pickInfo.pickedMesh !== sphere) {
-			// 	gridMaterial.circleAmount = 0
-			// 	return
-			// }
+			scene.onPointerMove = (
+				event: BABYLON.IPointerEvent,
+				pickInfo: BABYLON.PickingInfo
+			) => {
+				if (pickInfo.pickedMesh !== sphere) {
+					circleAmount.value = 0
+					return
+				}
 
-			if (pickInfo.pickedPoint === null) {
-				return
+				const pointerPosition = pickInfo.pickedPoint.subtract(sphere.position)
+				const gridFactor =
+					1 / gridRatio.value / Math.round(majorUnitFrequency.value)
+				const jointPosition = pointerPosition.scale(gridFactor)
+				jointPosition.x = Math.round(jointPosition.x)
+				jointPosition.y = Math.round(jointPosition.y)
+				jointPosition.z = Math.round(jointPosition.z)
+				jointPosition.scaleInPlace(1 / gridFactor)
+
+				const jointPositionSqrd = jointPosition.multiply(jointPosition)
+
+				const cutoff = 0.5 * sphereRadius
+				const absX = Math.abs(jointPosition.x)
+				const absY = Math.abs(jointPosition.y)
+				const absZ = Math.abs(jointPosition.z)
+				const max = Math.max(Math.max(absX, absY, absZ))
+				if (max === absX) {
+					jointPosition.x =
+						Math.sign(jointPosition.x) *
+						Math.sqrt(
+							sphereRadiusSqrd - jointPositionSqrd.y - jointPositionSqrd.z
+						)
+					circleAmount.value = absY > cutoff || absZ > cutoff ? 0 : 1
+				} else if (max === absY) {
+					jointPosition.y =
+						Math.sign(jointPosition.y) *
+						Math.sqrt(
+							sphereRadiusSqrd - jointPositionSqrd.x - jointPositionSqrd.z
+						)
+					circleAmount.value = absX > cutoff || absZ > cutoff ? 0 : 1
+				} else {
+					jointPosition.z =
+						Math.sign(jointPosition.z) *
+						Math.sqrt(
+							sphereRadiusSqrd - jointPositionSqrd.x - jointPositionSqrd.y
+						)
+					circleAmount.value = absX > cutoff || absY > cutoff ? 0 : 1
+				}
+
+				console.log(jointPosition)
+
+				circlePosition.value = jointPosition
+				if (
+					BABYLON.Vector3.Distance(pointerPosition, jointPosition) >
+					circleRadius.value
+				) {
+					circleAmount.value = 0
+				}
 			}
-
-			console.log(pickInfo.pickedPoint)
-
-			const pointerPosition = pickInfo.pickedPoint.subtract(sphere.position)
-			const gridFactor =
-				1 / gridMaterial.gridRatio / Math.round(gridMaterial.majorUnitFrequency)
-			const jointPosition = pointerPosition.scale(gridFactor)
-			jointPosition.x = Math.round(jointPosition.x)
-			jointPosition.y = Math.round(jointPosition.y)
-			jointPosition.z = Math.round(jointPosition.z)
-			jointPosition.scaleInPlace(1 / gridFactor)
-			const jointPositionSqrd = jointPosition.multiply(jointPosition)
-			const cutoff = 0.5 * sphereRadius
-			const absX = Math.abs(jointPosition.x)
-			const absY = Math.abs(jointPosition.y)
-			const absZ = Math.abs(jointPosition.z)
-			const max = Math.max(Math.max(absX, absY, absZ))
-			if (max === absX) {
-				jointPosition.x =
-					Math.sign(jointPosition.x) *
-					Math.sqrt(
-						sphereRadiusSqrd - jointPositionSqrd.y - jointPositionSqrd.z
-					)
-				// circleAmount.value = absY > cutoff || absZ > cutoff ? 0 : 1
-			} else if (max === absY) {
-				jointPosition.y =
-					Math.sign(jointPosition.y) *
-					Math.sqrt(
-						sphereRadiusSqrd - jointPositionSqrd.x - jointPositionSqrd.z
-					)
-				// circleAmount.value = absX > cutoff || absZ > cutoff ? 0 : 1
-			} else {
-				jointPosition.z =
-					Math.sign(jointPosition.z) *
-					Math.sqrt(
-						sphereRadiusSqrd - jointPositionSqrd.x - jointPositionSqrd.y
-					)
-				// circleAmount.value = absX > cutoff || absY > cutoff ? 0 : 1
-			}
-			// circlePosition.value = jointPosition
-			// if (
-			// 	BABYLON.Vector3.Distance(pointerPosition, jointPosition) >
-			// 	circleRadius.value
-			// ) {
-			// 	circleAmount.value = 0
-			// }
-		}
+		})
 
 		engine.runRenderLoop(() => {
 			scene.render()
