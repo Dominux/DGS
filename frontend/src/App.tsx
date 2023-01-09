15b4a33aa6ddd9f8c9d084e9c649e01674bbe795
Dominux <kir.gimranov@gmail.com>
2023-01-09 14:23:15 +0300

    #39 Started creating UI for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { Route, Routes } from '@solidjs/router'
 import { Component, onMount } from 'solid-js'
 
 import styles from './App.module.css'
 import { SPHERE_RADIUS } from './constants'
 import GameManager from './logic/game_manager'
+import ModeChooser from './pages/ModeChooser'
 
 const App: Component = () => {
 	onMount(() => {
@@ -16,6 +18,15 @@ const App: Component = () => {
 		<div class={styles.App}>
 			{/* Game Canvas */}
 			<canvas ref={canvas} class={styles.canvas}></canvas>
+
+			<div class={styles.UI}>
+				<h1>LMAO</h1>
+				<Routes>
+					<Route path="/" component={ModeChooser} />
+					<Route path="/game/singleplayer" element={<div>Lmao</div>} />
+					<Route path="/game/multiplayer" element={<div>Lmao</div>} />
+				</Routes>
+			</div>
 		</div>
 	)
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import suidPlugin from '@suid/vite-plugin'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
-});
+	plugins: [suidPlugin(), solidPlugin()],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
+})
