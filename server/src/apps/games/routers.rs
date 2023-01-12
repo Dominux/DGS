7688c2047a1dfb3454ed0bbd65bd1d6a2d9b07e8
Dominux <kir.gimranov@gmail.com>
2023-01-13 02:13:30 +0300

    #39 on my way to implement ws on server

--- server/src/apps/games/routers.rs
+++ server/src/apps/games/routers.rs
@@ -1,6 +1,15 @@
 use std::sync::Arc;
 
-use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use axum::{
+    extract::{
+        ws::{WebSocket, WebSocketUpgrade},
+        State,
+    },
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+    Json, Router,
+};
 
 use super::{schemas::CreateGameSchema, services::GameService};
 use crate::common::routing::{app_state::AppState, auth::AuthenticatedUser};
@@ -11,10 +20,11 @@ impl GamesRouter {
     pub fn get_router(state: Arc<AppState>) -> Router {
         Router::new()
             .route("/", post(Self::start_game))
+            .route("/ws", get(Self::ws_handler))
             .with_state(state)
     }
 
-    pub async fn start_game(
+    async fn start_game(
         State(state): State<Arc<AppState>>,
         user: AuthenticatedUser,
         Json(schema): Json<CreateGameSchema>,
@@ -22,4 +32,10 @@ impl GamesRouter {
         let game_with_link = GameService::new(&state.db).start_game(schema, user).await?;
         Ok::<_, (StatusCode, String)>((StatusCode::CREATED, Json(game_with_link)))
     }
+
+    async fn ws_handler(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> Response {
+        ws.on_upgrade(|socket| Self::websocket(socket, state))
+    }
+
+    async fn websocket(stream: WebSocket, state: Arc<AppState>) {}
 }
