c191c368a32085723b65469566b3c461ce8a6609
Dominux <kir.gimranov@gmail.com>
2023-01-19 01:51:28 +0300

    #39 FINALLY DONE MF

--- server/src/app.rs
+++ server/src/app.rs
@@ -1,15 +1,6 @@
 use std::sync::Arc;
 
-use axum::{
-    extract::{
-        ws::{Message, WebSocket},
-        WebSocketUpgrade,
-    },
-    response::IntoResponse,
-    routing::get,
-    Router,
-};
-use futures::{SinkExt, StreamExt};
+use axum::Router;
 use tower_http::{
     cors,
     trace::{self, TraceLayer},
@@ -35,7 +26,6 @@ pub fn create_app(app_state: Arc<AppState>) -> Router {
         );
 
     Router::new()
-        .route("/websocket", get(websocket_handler))
         .nest("/users", UsersRouter::get_router(app_state.clone()))
         .nest("/games", GamesRouter::get_router(app_state.clone()))
         .nest("/rooms", RoomsRouter::get_router(app_state))
@@ -45,23 +35,4 @@ pub fn create_app(app_state: Arc<AppState>) -> Router {
                 .on_response(trace::DefaultOnResponse::new().level(Level::INFO)),
         )
     // .layer(dgs_cors)
-}
-
-async fn websocket_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
-    println!("inside websocket handler");
-    ws.on_upgrade(|socket| websocket(socket))
-}
-
-async fn websocket(stream: WebSocket) {
-    // By splitting, we can send and receive at the same time.
-    let (mut sender, mut _receiver) = stream.split();
-    println!("inside websocket");
-
-    let _ = sender
-        .send(Message::Text(
-            "lmao sup nibba, connection acquired".to_string(),
-        ))
-        .await;
-
-    println!("after sending msg");
 }
