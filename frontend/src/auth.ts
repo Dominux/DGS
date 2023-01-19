9d3412bb3e34d24a35a31a07f4cf65715419d42b
Dominux <kir.gimranov@gmail.com>
2023-01-19 16:38:27 +0300

    Fix

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,4 +1,4 @@
-import { useNavigate } from '@solidjs/router'
+import { useLocation, useNavigate } from '@solidjs/router'
 
 import createLocalStore from '../libs'
 import { fullLocation } from './router'
@@ -6,9 +6,10 @@ import { fullLocation } from './router'
 export function checkAuth() {
 	const [store, setStore] = createLocalStore()
 	const navigate = useNavigate()
+	const location = useLocation()
 
 	if (!store.user) {
-		setStore('redirect', window.location.href)
+		setStore('redirect', location.pathname)
 
 		navigate(fullLocation('/login'))
 	}
