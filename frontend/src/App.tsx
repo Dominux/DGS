ac89f89594d2b8d8db78973e85a2daf3650b7047
Dominux <kir.gimranov@gmail.com>
2023-01-20 19:17:36 +0300

    #41 Added home button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,38 @@
 import { Component } from 'solid-js'
-import { useRoutes } from '@solidjs/router'
+import { useNavigate, useRoutes } from '@solidjs/router'
+import { Button, Card, CardActions, IconButton } from '@suid/material'
+import { House } from '@suid/icons-material'
 
 import styles from './App.module.css'
-import routes from './router'
+import routes, { fullLocation } from './router'
 import { BASENAME } from './constants'
 
 const App: Component = () => {
 	const Routes = useRoutes(routes, BASENAME)
+	const navigate = useNavigate()
 
 	return (
 		<div class={styles.App}>
+			<Card
+				sx={{
+					position: 'absolute',
+					top: '1.5%',
+					left: '1.5%',
+					zIndex: 2,
+					width: 'fit-content',
+					borderRadius: '33%',
+				}}
+			>
+				<CardActions sx={{ padding: 0 }}>
+					<IconButton
+						color="primary"
+						onClick={() => navigate(fullLocation('/'))}
+					>
+						<House fontSize="large" />
+					</IconButton>
+				</CardActions>
+			</Card>
+
 			<Routes />
 		</div>
 	)
